13fe3e691a8acaaae5316b699daa265061716afe
Melika Dabiri <jalehdabiri@gmail.com>
2025-02-28 15:18:31 -0800

    routing - navbar works

--- packages/routing-lab/routing-lab/src/App.jsx
+++ packages/routing-lab/routing-lab/src/App.jsx
@@ -2,16 +2,27 @@ import { Homepage } from "./Homepage";
 import { AccountSettings } from "./AccountSettings";
 import { ImageGallery } from "./images/ImageGallery.jsx";
 import { ImageDetails } from "./images/ImageDetails.jsx";
+import { Routes, Route } from "react-router";
 
 function App() {
-    const POSSIBLE_PAGES = [
-        <Homepage userName="John Doe" />,
-        <AccountSettings />,
-        <ImageGallery />,
-        <ImageDetails imageId="0" />
-    ]
+    // const POSSIBLE_PAGES = [
+    //     <Homepage userName="John Doe" />,
+    //     <AccountSettings />,
+    //     <ImageGallery />,
+    //     <ImageDetails imageId="0" />
+    // ]
 
-    return POSSIBLE_PAGES[0];
+    // return POSSIBLE_PAGES[0];
+    return(
+        <Routes>
+            <Route path='/' element={<Homepage username="John Doe" />}/>
+            <Route path='/account' element={<AccountSettings />}/>
+            <Route path='/images' element={<ImageGallery />}/>
+            {/* <Route path='/' element={<ImageDetails imageId="0" />}/> */}
+
+            
+        </Routes>
+    )
 }
 
 export default App
