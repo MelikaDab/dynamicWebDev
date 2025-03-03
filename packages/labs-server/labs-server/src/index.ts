ab2925e904f156f8b8cd110a00aa542638453045
Melika Dabiri <mdabiri@edgeautonomy.io>
2025-03-03 15:28:04 -0800

    mongodb setup + express backend

---
+++ packages/labs-server/labs-server/src/index.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+
+dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
+const PORT = process.env.PORT || 3000;
+const staticDir = process.env.STATIC_DIR || "public";
+
+async function setUpServer() {
+
+  const { MONGO_USER, MONGO_PWD, MONGO_CLUSTER, DB_NAME } = process.env;
+  
+  const connectionStringRedacted = `mongodb+srv://${MONGO_USER}:<password>@${MONGO_CLUSTER}/${DB_NAME}`;
+  const connectionString = `mongodb+srv://${MONGO_USER}:${MONGO_PWD}@${MONGO_CLUSTER}/${DB_NAME}`;
+  
+  console.log("Attempting Mongo connection at " + connectionStringRedacted);
+  
+  const mongoClient = await MongoClient.connect(connectionString);
+  const collectionInfos = await mongoClient.db().listCollections().toArray();
+  // console.log(collectionInfos.map(collectionInfo => collectionInfo.name)); // For debug only
+}
+
+const app = express();
+app.use(express.static(staticDir));
+
+app.get("/hello", (req: Request, res: Response) => {
+    res.send("Hello, World");
+});
+
+app.get("*", (req: Request, res: Response) => {
+  console.log("none of the routes above me were matched");
+  res.sendFile("index.html", { root: staticDir });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+
+setUpServer();
