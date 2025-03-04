c8314286215d35d8fe2e6280619445cd8c745f56
Melika Dabiri <mdabiri@edgeautonomy.io>
2025-03-03 23:21:47 -0800

    making a new route that fetches images

--- packages/labs-server/labs-server/src/index.ts
+++ packages/labs-server/labs-server/src/index.ts
@@ -1,11 +1,13 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, Express } from "express";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
-import { ImageProvider } from "ImageProvider";
+import { ImageProvider } from "./ImageProvider";
 
 dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000;
 const staticDir = process.env.STATIC_DIR || "public";
+
+let mongoClient: MongoClient; 
 
 async function setUpServer() {
 
@@ -16,21 +18,31 @@ async function setUpServer() {
   
   console.log("Attempting Mongo connection at " + connectionStringRedacted);
   
-  const mongoClient = await MongoClient.connect(connectionString);
+  mongoClient = await MongoClient.connect(connectionString);
   const collectionInfos = await mongoClient.db().listCollections().toArray();
   // console.log(collectionInfos.map(collectionInfo => collectionInfo.name)); // For debug only
 }
 
-const app = express();
+const app : Express = express();
 app.use(express.static(staticDir));
 
 app.get("/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
 });
 
+app.get("/api/images", (req: Request, res: Response) => {})
+
 app.get("/api/images", (req: Request, res: Response) => {
-  
-})
+  // if (!mongoClient) {
+  //   return res.status(500).send("Database not initialized");
+  // }
+
+  const imageProvider = new ImageProvider(mongoClient);
+  imageProvider.getAllImages()
+    .then(images => res.json(images))
+    .catch(error => res.status(500).json({ error: error.message }));
+});
+
 
 app.get("*", (req: Request, res: Response) => {
   console.log("none of the routes above me were matched");
